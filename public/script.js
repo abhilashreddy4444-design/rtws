d50bc417a73ff2360a9ac2b64ab2d2c307d2b483
abhilashreddy4444-design <abhilashreddy4444@gmail.com>
2026-01-10 17:01:16 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
+const socket = io();
+
+function sendMessage() {
+  const input = document.getElementById("message");
+  const msg = input.value;
+
+  if (msg.trim() !== "") {
+    socket.emit("message", msg);
+    input.value = "";
+  }
+}
+
+socket.on("message", (msg) => {
+  const chatBox = document.getElementById("chat-box");
+  const p = document.createElement("p");
+  p.textContent = msg;
+  chatBox.appendChild(p);
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
 
